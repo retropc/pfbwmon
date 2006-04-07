19497de82ae6215dc51232a95151ddb086146ee1
slug <chris@warp13.co.uk>
2006-04-07 22:12:00 +0100

    Moved remotely

---
+++ pfbwmong/trunk/pfbwmonc.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python
+from socket import *
+import sys
+import struct
+import os
+import rrdtool
+
+def daemonise():
+  pid = os.fork()
+  if pid:
+    sys.exit(0)
+
+  os.setsid()
+  os.umask(0)
+  os.close(sys.stdin.fileno())
+  os.close(sys.stdout.fileno())
+  os.close(sys.stderr.fileno())
+  return pid
+
+def dropprivs(user):
+  import pwd
+  if os.getuid() != 0:
+    return
+
+  pw = pwd.getpwnam(user)
+  if not pw:
+    sys.exit(1)
+
+  os.chroot(pw[5]);
+  os.chdir('/')
+
+  os.setgroups([pw[3]])
+  os.setgid(pw[3])
+  os.setegid(pw[3])
+  os.setuid(pw[2])
+  os.seteuid(pw[2])
+
+if len(sys.argv) == 2:
+  configname = sys.argv[1]
+else:
+  configname = 'pfbwmon.conf'
+
+try:
+ config = eval(open(configname).read())
+except IOError:
+  print("Unable to open %s!" % configname)
+  sys.exit(1)
+
+if config.has_key('user'):
+  dropprivs(config['user'])
+
+buf = config['buf']
+addr = config['addr']
+fromaddr = config['fromaddr']
+hosts = config['hosts']
+rrdfile = config['rrdfile']
+
+choice = 2000
+
+if not os.path.exists(rrdfile):
+  output = 'rrdtool.create(rrdfile, "--start", "N", "--step", "5", '
+  for i in range(0, len(hosts)):
+    output += "'DS:%sin:COUNTER:10:U:U', 'DS:%sout:COUNTER:10:U:U', " % (hosts[i][0], hosts[i][0])
+  output += '"RRA:AVERAGE:0.5:1:1000", "RRA:AVERAGE:0.5:15:1250", "RRA:AVERAGE:0.5:100:1500", "RRA:AVERAGE:0.5:400:2000", "RRA:AVERAGE:0.5:6000:3000", "RRA:AVERAGE:0.5:60000:5000")'
+  eval(output)
+
+mappings = {}
+
+for i in range(0, len(hosts)):
+  ips = hosts[i][1]
+  for j in range(0, len(ips)):
+    mappings[ips[j]] = i
+
+uso = socket(AF_INET, SOCK_DGRAM)
+uso.bind(addr)
+
+daemonise()
+
+blah = -1
+while 1:
+  data, addr = uso.recvfrom(1024)
+  if data and addr[0] == fromaddr:
+    items = ord(data[0])
+    data = data[1:]
+    
+    hash = []
+    keys = mappings.keys()
+    max = 0
+    for key in keys:
+      if mappings[key] > max:
+        max = mappings[key]
+
+    for i in range(0, max + 1):
+      hash.insert(0, (0, 0))
+
+    for i in range(0, items):
+      ourdata = data[i * 20:i * 20 + 20]
+      iin = struct.unpack("!Q", ourdata[4:12])[0]
+      iout = struct.unpack("!Q", ourdata[12:20])[0]
+      ip = inet_ntoa(ourdata[:4])
+      if mappings.has_key(ip):
+        pos = mappings[ip]
+        hash[pos] = (hash[pos][0] + iin, hash[pos][1] + iout)
+
+    output = "N:"
+    while len(hash) > 0:
+      item = hash.pop(0)
+      output += "%d:%d:" % (item[0], item[1])
+    rrdtool.update(rrdfile, output[:-1])
+
---
+++ pfbwmong/trunk/pfbwmonw.py
@@ -0,0 +1,109 @@
+#!/usr/bin/env python
+from socket import *
+import sys
+import struct
+import os
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+
+permitted = []
+
+class handler(BaseHTTPRequestHandler):
+  def do_GET(self):
+    page = self.path
+    try:
+
+      if page and (page[0] == '/'):
+        page = page[1:]
+
+      if page == '':
+        page = 'index.html'
+
+      l = permitted[page]
+      f = open(page)
+
+      self.send_response(200)
+      self.send_header('Content-type', l)
+      self.end_headers()
+      self.wfile.write(f.read())
+      f.close()
+
+    except:
+      self.ourerror(404, "File not found")
+    
+    return
+
+  def do_POST(self):
+    self.ourerror(403)
+
+  def ourerror(self, code, text):
+    self.send_response(code)
+    self.send_header('Content-type', 'text/html')
+    self.end_headers()
+    self.wfile.write('<html><head><title>Error %d</title></head><body><h1>Error %d</h1><p>%s</p></body></html>' % (code, code, text))
+    return
+
+def daemonise():
+  pid = os.fork()
+  if pid:
+    sys.exit(0)
+
+  sys.stdin.close()
+  sys.stdout.close()
+  sys.stderr.close()
+
+  sys.stdin = stdin
+  sys.stdout = stdout
+  sys.stderr = stderr
+
+  os.setsid()
+  os.umask(0)
+
+  return pid
+
+def dropprivs(user):
+  import pwd
+  if os.getuid() != 0:
+    return
+
+  pw = pwd.getpwnam(user)
+  if not pw:
+    sys.exit(1)
+
+  os.chroot(pw[5]);
+  os.chdir('/')
+
+  os.setgroups([pw[3]])
+  os.setgid(pw[3])
+  os.setegid(pw[3])
+  os.setuid(pw[2])
+  os.seteuid(pw[2])
+
+if len(sys.argv) == 2:
+  configname = sys.argv[1]
+else:
+  configname = 'pfbwmon.conf'
+
+try:
+ config = eval(open(configname).read())
+except IOError:
+  print("Unable to open %s!" % configname)
+  sys.exit(1)
+
+stdin = open('/dev/null')      
+stdout = open('/dev/null', 'w')
+stderr = open('/dev/null', 'w')
+
+dropprivs(config['wwwuser'])
+
+permitted = config['wwwpages']
+
+port = 5580
+
+if config.has_key('wwwport'):
+  port = int(config['wwwport'])
+
+server = HTTPServer(('', port), handler)
+
+daemonise()
+
+server.serve_forever()
